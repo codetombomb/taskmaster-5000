b59483bf3a6da8b33b83446dcb16db995985f2d3
Tom Tobar <codetombomb@gmail.com>
2023-08-06 03:22:12 -0500

    Update seed.py

--- lib/seed.py
+++ lib/seed.py
@@ -43,48 +43,6 @@ for user in all_users:
 
 
 if __name__ == "__main__":
-    
-#  SQLAlchemy query with common filters https://rimsovankiry.medium.com/sqlalchemy-query-with-common-filters-c7adbd3321a6
-    
-# Retrieve all rows from a table. - session.query(User).all()
-
-# Retrieve a specific row by primary key. 
-#   - session.query(User).get(10)
-#  Alternate - session.query(User).filter_by(id=10).first()
-# ---------------------------
-
-# Filter rows based on a column value. 
-# - session.query(User).filter(User.last_name.like("W%")).all() 
-# - The % sign is a wildcard character, meaning any characters can come after "W".
-# ---------------------------
-
-# Retrieve all rows from the User table where the birthday column is between two given dates, for example, between '1990-01-01' and '2000-12-31'.
-# - session.query(User).filter(User.birthday.between("1990-01-01", "2000-12-31")).all()
-# ---- ALTERNATIVE ----
-
-# from datetime import datetime
-
-# start_date = datetime(1990, 1, 1)
-# end_date = datetime(2000, 12, 31)
-
-# result = session.query(User).filter(User.birthday >= start_date, User.birthday <= end_date).all()
-
-# ---------------------------
-
-# Filter rows using multiple conditions (AND, OR).
-
-# Query with case-insensitive filtering. session.query(User).filter(User.last_name.like("W%")).all() 
-
-# Ordering query results.
-
-# Limit the number of rows returned.
-
-# Using the IN operator to filter by multiple values.
-
-# Count the number of rows that match a condition.
-
-# Performing aggregate functions (e.g., SUM, AVG) on a column.
-
     import ipdb
 
     ipdb.set_trace()
